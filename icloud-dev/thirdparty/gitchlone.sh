1343bbd8e40396212c9e5ce66bbd5442431d726f
cuijiangning <jiangning.cui@travelzen.com>
2014-11-24 09:22:14 +0800

    加入thirdparty脚本

---
+++ icloud-dev/thirdparty/gitchlone.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/hibernate/hibernate-tools.git
+git clone https://github.com/apache/isis.git
+git clone https://github.com/tobiasflohre/movie-database
+git clone https://github.com/spring-projects/spring-hateoas.git
