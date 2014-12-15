dcb5a6ff002a04bb37d1f4c58acb18c4b94525d2
cuipafa <cuijiangning08@163.com>
2014-12-15 11:25:01 +0800

    longest palindrome

--- icloud-leetcode/icloud-leetcode-test/src/main/java/com/icloud/leetcode/string/StringSolution.java
+++ icloud-leetcode/icloud-leetcode-test/src/main/java/com/icloud/leetcode/string/StringSolution.java
@@ -43,6 +43,35 @@ public class StringSolution {
 	}
 
 	/**
+	 * 最长回文子串 回文
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public String longestPalindrome(String s) {
+		if (s == null || s.length() == 0)
+			return s;
+		int n = s.length();
+		String maxS = "";
+		for (int i = 0; i < n; i++) {
+			String tmp1 = expandPalindrome(s, i, i);
+			String tmp2 = expandPalindrome(s, i, i + 1);
+			tmp1 = tmp1.length() > tmp2.length() ? tmp1 : tmp2;
+			maxS = tmp1.length() > maxS.length() ? tmp1 : maxS;
+		}
+		return maxS;
+	}
+
+	private String expandPalindrome(String s, int l, int r) {
+		int n = s.length();
+		while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+			l--;
+			r++;
+		}
+		return s.substring(l + 1, r);
+	}
+
+	/**
 	 * 获得最常的子串的长度，沒有重複
 	 * 
 	 * @param args
@@ -52,6 +81,8 @@ public class StringSolution {
 		String s = "afddsafasf";
 		int len = solution.lengthOfLongestSubstring(s);
 		System.out.println(len);
+		String s1 = solution.expandPalindrome(s, 2, 3);
+		System.out.println(s1);
 	}
 
 }
