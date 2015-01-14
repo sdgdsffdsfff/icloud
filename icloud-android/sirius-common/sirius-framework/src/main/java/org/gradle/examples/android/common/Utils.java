31f104b0d0323577a86636ad2dfad02449bd0819
cuijiangning <jiangning.cui@travelzen.com>
2015-01-14 17:22:28 +0800

    add android project

---
+++ icloud-android/sirius-common/sirius-framework/src/main/java/org/gradle/examples/android/common/Utils.java
@@ -0,0 +1,13 @@
+package org.gradle.examples.android.common;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Utils {
+	public static String capitalize(String s) {
+		return StringUtils.capitalize(s);
+	}
+
+	public static String get() {
+		return "q111";
+	}
+}
